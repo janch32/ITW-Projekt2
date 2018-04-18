0fdd936052b327244355422aec67d879c5ae0955
Jan Chaloupka <janch32@gmail.com>
2018-04-18 23:22:32 +0200

    Přidány sekce brno, kontakt, reference na index, upravena patička

--- assets/main-script.js
+++ assets/main-script.js
@@ -1,9 +1,11 @@
 var heroPanel = document.getElementById("hero");
+var brnoPanel = document.getElementById("brno");
 var header = document.getElementsByTagName("header")[0];
 
 header.style.position = "fixed";
 
 window.addEventListener("scroll", function(){
 	if(heroPanel) heroPanel.style.backgroundPositionY = "center, " + (window.scrollY/2) + "px";
+	/*if(brnoPanel)*/ brnoPanel.style.backgroundPositionY = ((window.scrollY - brnoPanel.offsetTop - 400)/2) + "px";
 	header.className = window.scrollY > 10 ? "floating" : "";
 });

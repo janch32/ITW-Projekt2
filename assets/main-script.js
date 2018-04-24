e5556859daaa7666ec03d8872b10f87e82daa4c1
Jan Chaloupka <janch32@gmail.com>
2018-04-24 22:56:28 +0200

    Předěláno menu na dropdown + template

--- assets/main-script.js
+++ assets/main-script.js
@@ -6,6 +6,6 @@ header.style.position = "fixed";
 
 window.addEventListener("scroll", function(){
 	if(heroPanel) heroPanel.style.backgroundPositionY = "center, " + (window.scrollY/2) + "px";
-	/*if(brnoPanel)*/ brnoPanel.style.backgroundPositionY = ((window.scrollY - brnoPanel.offsetTop - 400)/2) + "px";
+	if(brnoPanel) brnoPanel.style.backgroundPositionY = ((window.scrollY - brnoPanel.offsetTop-150)/2) + "px";
 	header.className = window.scrollY > 10 ? "floating" : "";
 });

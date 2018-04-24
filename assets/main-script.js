b9aa005aae8bbc1bdae673933b856dd0be7cd1ee
Jan Chaloupka <janch32@gmail.com>
2018-04-25 01:30:49 +0200

    Hotovo celá sekce O nás

--- assets/main-script.js
+++ assets/main-script.js
@@ -1,5 +1,6 @@
 var heroPanel = document.getElementById("hero");
 var brnoPanel = document.getElementById("brno");
+var careerPanel = document.getElementById("career");
 var header = document.getElementsByTagName("header")[0];
 
 header.style.position = "fixed";
@@ -7,5 +8,6 @@ header.style.position = "fixed";
 window.addEventListener("scroll", function(){
 	if(heroPanel) heroPanel.style.backgroundPositionY = "center, " + (window.scrollY/2) + "px";
 	if(brnoPanel) brnoPanel.style.backgroundPositionY = ((window.scrollY - brnoPanel.offsetTop-150)/2) + "px";
+	if(careerPanel) careerPanel.style.backgroundPositionY = ((window.scrollY - careerPanel.offsetTop)/2) + "px";
 	header.className = window.scrollY > 10 ? "floating" : "";
 });

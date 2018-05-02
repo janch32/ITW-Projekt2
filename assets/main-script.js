cdd0aba09998a40b02882942d173e502a1ea7ec4
Jan Chaloupka <janch32@gmail.com>
2018-05-02 16:21:16 +0200

    Úprava vzhledu nabídek

--- assets/main-script.js
+++ assets/main-script.js
@@ -5,9 +5,22 @@ var header = document.getElementsByTagName("header")[0];
 
 header.style.position = "fixed";
 
-window.addEventListener("scroll", function(){
-	if(heroPanel) heroPanel.style.backgroundPositionY = "center, " + (window.scrollY/2) + "px";
-	if(brnoPanel) brnoPanel.style.backgroundPositionY = ((window.scrollY - brnoPanel.offsetTop-150)/2) + "px";
-	if(careerPanel) careerPanel.style.backgroundPositionY = ((window.scrollY - careerPanel.offsetTop)/2) + "px";
+
+var onScroll = function(){
 	header.className = window.scrollY > 10 ? "floating" : "";
-});
+}
+
+if(	window.navigator.userAgent.indexOf("Trident")<0 &&
+	window.navigator.userAgent.indexOf("Edge")<0){
+	var originalonScroll = onScroll;
+	onScroll = function(){
+		originalonScroll();
+		if(heroPanel) heroPanel.style.backgroundPositionY = "center, " + (window.scrollY/2) + "px";
+		if(brnoPanel) brnoPanel.style.backgroundPositionY = ((window.scrollY - brnoPanel.offsetTop-150)/2) + "px";
+		if(careerPanel) careerPanel.style.backgroundPositionY = ((window.scrollY - careerPanel.offsetTop)/2) + "px";
+	}
+}
+
+window.addEventListener("scroll", onScroll);
+window.addEventListener("load", onScroll);
+//onScroll();
